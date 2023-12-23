6291174e6cb0082cd7935771a065470776baf7a6
Mac <mgelvoleo@gmail.com>
2023-12-24 01:35:23 +0800

    Create variale.tf

---
+++ variale.tf
@@ -0,0 +1,17 @@
+variable "os" {
+  type = string
+  default = "ami-078c1149d8ad719a7"
+  description = "ami for ubuntu"
+}
+
+variable "size" {
+  default = "t2.micro"
+}
+
+/* variable "bucketname" {
+  
+}
+
+variable "username" {
+  
+} */
